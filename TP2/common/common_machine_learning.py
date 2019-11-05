125fbe3260850e5169008f7cb24402cd98e76741
Franco Martin Di Maria <franki.dimaria@gmail.com>
2019-11-05 17:17:54 -0300

    Agrego archivo common donde colocar funciones comunes utiles

---
+++ TP2/common/common_machine_learning.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+def cargar_set_optimizado(ruta_set_datos):
+    """
+    PRE: Recibe la ruta al el set de datos
+    'train.csv', alguno derivado pero que
+    mantenga todas sus columnas.
+    POST: Carga el set de datos en un dataframe
+    (pandas.DataFrame), optimizando los tipos
+    de datos, para ahorrar espacio en memoria.
+    Devuelve este dataframe (pandas.DataFrame)
+    """
+    df_optimizado = pd.read_csv(ruta_set_datos, \
+                        dtype={ \
+                            'id': np.int32, \
+                            #'tipodepropiedad': 'category', \
+                            #'provincia': 'category', \
+                            #'ciudad': 'category', \
+                            'antiguedad': np.float16, \
+                            'habitaciones': np.float16, \
+                            'garages': np.float16, \
+                            'banos': np.float16, \
+                            'metroscubiertos': np.float16, \
+                            'metrostotales': np.float16, \
+                            'idzona': np.float32, \
+                            'gimnasio': 'bool', \
+                            'usosmultiples': 'bool', \
+                            'piscina': 'bool', \
+                            'escuelascercanas': 'bool', \
+                            'centroscomercialescercanos': 'bool', \
+                            'precio': np.float32 \
+                            },
+                        parse_dates = ['fecha'],
+                        date_parser = pd.to_datetime
+                        )
+    return df_optimizado
